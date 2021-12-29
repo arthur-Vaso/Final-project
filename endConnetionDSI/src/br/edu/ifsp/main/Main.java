03e193fe700fbc5a0e0c784835e88777a65d131e
Arthur Orlando Vaso <eng.arthurvaso@outlook.com>
2021-12-29 11:24:45 -0300

    Initial work.

---
+++ endConnetionDSI/src/br/edu/ifsp/main/Main.java
@@ -0,0 +1,11 @@
+package br.edu.ifsp.main;
+
+import br.edu.ifsp.windows.MainWindow;
+
+public class Main {
+
+	public static void main(String[] args) {
+		MainWindow mw = new MainWindow();
+	}
+
+}
--- endConnetionDSI/src/br/edu/ifsp/view/Main.java
+++
@@ -1,9 +0,0 @@
-package br.edu.ifsp.view;
-
-public class Main {
-
-	public static void main(String[] args) {
-		
-	}
-
-}
