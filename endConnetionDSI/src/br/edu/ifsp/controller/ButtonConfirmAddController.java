6d8759cedc7a7edc850458f977007ff575790b8b
Arthur Orlando Vaso <eng.arthurvaso@outlook.com>
2022-01-06 20:01:31 -0300

    Added GameDAO class to integrate java and DB, added new windows with their calls, including the call to gameDAO.

--- endConnetionDSI/src/br/edu/ifsp/controller/ButtonConfirmAddController.java
+++ endConnetionDSI/src/br/edu/ifsp/controller/ButtonConfirmAddController.java
@@ -5,8 +5,11 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JOptionPane;
 
+import br.edu.ifsp.gameDAO.GameDAO;
+import br.edu.ifsp.model.Game;
 import br.edu.ifsp.windows.MainWindow;
 import br.edu.ifsp.windows.WindowAdd;
+import br.edu.ifsp.windows.WindowDelete;
 import br.edu.ifsp.windows.WindowEdit;
 
 public class ButtonConfirmAddController implements ActionListener {
@@ -14,23 +17,36 @@ public class ButtonConfirmAddController implements ActionListener {
 	private MainWindow mainWindow;
 	private WindowAdd windowAdd;
 	private WindowEdit windowEdit;
-	
+
 	private int optionControler;
+	private GameDAO gameDao;
+	private WindowDelete windowDelete;
 
 	public ButtonConfirmAddController(WindowAdd windowAdd, MainWindow mainWindow) {
 		this.mainWindow = mainWindow;
-		
+
 		this.windowAdd = windowAdd;
 		this.windowAdd.getBtnConfirm().addActionListener(this);
 
 		optionControler = 0;
 	}
 
-	public ButtonConfirmAddController(WindowEdit windowEdit) {
+	public ButtonConfirmAddController(WindowEdit windowEdit, MainWindow mainWindow) {
+		this.mainWindow = mainWindow;
+
 		this.windowEdit = windowEdit;
 		this.windowEdit.getBtnConfirm().addActionListener(this);
 
 		optionControler = 1;
+	}
+
+	public ButtonConfirmAddController(WindowDelete windowDelete, MainWindow mainWindow) {
+		this.mainWindow = mainWindow;
+
+		this.windowDelete = windowDelete;
+		this.windowDelete.getBtnConfirm().addActionListener(this);
+
+		optionControler = 2;
 	}
 
 	@Override
@@ -42,6 +58,7 @@ public class ButtonConfirmAddController implements ActionListener {
 			if (cg.isBlank(windowAdd.getFldTitle(), windowAdd.getLblTitle(), " precisa ser preenchido.")
 					&& cg.isBlank(windowAdd.getFldDescription(), windowAdd.getLblDescription(),
 							" precisa ser preenchido.")
+					&& cg.isBlank(windowAdd.getFldHardware(), windowAdd.getLblHardware(), " precisa ser preenchido.")
 					&& cg.isBlank(windowAdd.getFldMinimumAge(), windowAdd.getLblMinimumAge(),
 							" precisa ser preenchido.")
 					&& cg.isBlank(windowAdd.getFldGenderOne(), windowAdd.getLblGenderOne(), " precisa ser preenchido.")
@@ -50,11 +67,29 @@ public class ButtonConfirmAddController implements ActionListener {
 					&& cg.isBlank(windowAdd.getFldAmount(), windowAdd.getLblAmount(),
 							" precisa ser preenchido números.")) {
 
-				System.out.println("Adicionado com sucesso!");
-				JOptionPane.showMessageDialog(null, windowAdd.getFldTitle().getText() + " adicionado com sucesso!");
+				Game g = new Game();
+				g.setTitle(windowAdd.getFldTitle().getText());
+				g.setDescription(windowAdd.getFldDescription().getText());
+				g.setHardware(windowAdd.getFldHardware().getText());
+				g.setMinimumAge(windowAdd.getFldMinimumAge().getText());
+				g.setGenderOne(windowAdd.getFldGenderOne().getText());
+				g.setGenderTwo(windowAdd.getFldGenderTwo().getText());
+				g.setPrice(windowAdd.getFldPrice().getText());
+				g.setAmount(windowAdd.getFldAmount().getText());
 
-				windowAdd.dispose();
-				mainWindow.setVisible(true);
+				gameDao = new GameDAO();
+				g = gameDao.saveGame(g);
+
+				if (g != null) {
+					System.out.println("diferente de nulo");
+					System.out.println("titulo: " + g.getTitle().toString());
+
+					System.out.println("Adicionado com sucesso!");
+					JOptionPane.showMessageDialog(null, windowAdd.getFldTitle().getText() + " adicionado com sucesso!");
+
+					windowAdd.dispose();
+					mainWindow.setVisible(true);
+				}
 			}
 			break;
 		}
@@ -62,6 +97,7 @@ public class ButtonConfirmAddController implements ActionListener {
 			if (cg.isBlank(windowEdit.getFldTitle(), windowEdit.getLblTitle(), " precisa ser preenchido.")
 					&& cg.isBlank(windowEdit.getFldDescription(), windowEdit.getLblDescription(),
 							" precisa ser preenchido.")
+					&& cg.isBlank(windowEdit.getFldHardware(), windowEdit.getLblHardware(), " precisa ser preenchido.")
 					&& cg.isBlank(windowEdit.getFldMinimumAge(), windowEdit.getLblMinimumAge(),
 							" precisa ser preenchido.")
 					&& cg.isBlank(windowEdit.getFldGenderOne(), windowEdit.getLblGenderOne(),
@@ -71,11 +107,45 @@ public class ButtonConfirmAddController implements ActionListener {
 					&& cg.isBlank(windowEdit.getFldAmount(), windowEdit.getLblAmount(),
 							" precisa ser preenchido números.")) {
 
-				System.out.println("Edição realizada com sucesso!");
-				JOptionPane.showMessageDialog(null, windowEdit.getFldTitle().getText() + " editado com sucesso!");
+				Game g = new Game();
+				g.setId(Integer.valueOf(windowEdit.getFldFetch().getText()));
+				g.setTitle(windowEdit.getFldTitle().getText());
+				g.setDescription(windowEdit.getFldDescription().getText());
+				g.setHardware(windowEdit.getFldHardware().getText());
+				g.setMinimumAge(windowEdit.getFldMinimumAge().getText());
+				g.setGenderOne(windowEdit.getFldGenderOne().getText());
+				g.setGenderTwo(windowEdit.getFldGenderTwo().getText());
+				g.setPrice(windowEdit.getFldPrice().getText());
+				g.setAmount(windowEdit.getFldAmount().getText());
 
-				windowEdit.dispose();
+				gameDao = new GameDAO();
+				g = gameDao.saveGame(g);
+
+				if (g != null) {
+					System.out.println("Edição realizada com sucesso!");
+					JOptionPane.showMessageDialog(null, windowEdit.getFldTitle().getText() + " editado com sucesso!");
+
+					windowEdit.dispose();
+					mainWindow.setVisible(true);
+				}
 			}
+			break;
+		}
+		case 2: {
+			if (JOptionPane.YES_OPTION == JOptionPane.showConfirmDialog(null,
+					"Deseja excluir " + windowDelete.getFldTitle().getText() + "?", "Alerta", JOptionPane.YES_NO_OPTION,
+					JOptionPane.WARNING_MESSAGE, null)) {
+
+				gameDao = new GameDAO();
+				gameDao.removeGame(Integer.valueOf(windowDelete.getFldFetch().getText()));
+
+				System.out.println("Exclusão realizada com sucesso!");
+				JOptionPane.showMessageDialog(null, windowDelete.getFldTitle().getText() + " excluido com sucesso!");
+
+				windowDelete.dispose();
+				mainWindow.setVisible(true);
+			}
+
 			break;
 		}
 		}
--- endConnetionDSI/src/br/edu/ifsp/controller/FetchControler.java
+++ endConnetionDSI/src/br/edu/ifsp/controller/FetchControler.java
@@ -3,8 +3,11 @@ package br.edu.ifsp.controller;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import br.edu.ifsp.gameDAO.GameDAO;
+import br.edu.ifsp.model.Game;
 import br.edu.ifsp.windows.InternalFrameFetch;
 import br.edu.ifsp.windows.InternalFrameName;
+import br.edu.ifsp.windows.WindowEdit;
 import br.edu.ifsp.windows.WindowFetch;
 import br.edu.ifsp.windows.WindowSelectFetch;
 
@@ -13,26 +16,43 @@ public class FetchControler implements ActionListener {
 	private InternalFrameFetch internalFrameFetch;
 	private WindowSelectFetch windowSelectFetch;
 	private WindowFetch windowFetch;
+	private WindowEdit windowEdit;
 
-	public FetchControler(InternalFrameFetch internalFrameFetch, WindowSelectFetch windowSelectFetch, WindowFetch windowFetch) {
+	public FetchControler(InternalFrameFetch internalFrameFetch, WindowSelectFetch windowSelectFetch,
+			WindowFetch windowFetch) {
 		this.windowSelectFetch = windowSelectFetch;
 		this.windowFetch = windowFetch;
-		
+
 		this.internalFrameFetch = internalFrameFetch;
 		this.internalFrameFetch.getBtnFetch().addActionListener(this);
+	}
+
+	public FetchControler(WindowEdit windowEdit) {
+		this.windowEdit = windowEdit;
+
+		this.windowEdit.getBtnFetch().addActionListener(this);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		CheckField cg = new CheckField();
-		if(cg.isBlank(internalFrameFetch.getFldFetch(), internalFrameFetch.getLblFetch(), " precisa ser preenchido.")) {
+		if (cg.isBlank(internalFrameFetch.getFldFetch(), internalFrameFetch.getLblFetch(),
+				" precisa ser preenchido.")) {
 			
-			this.internalFrameFetch.dispose();
-			this.windowFetch.dispose();
-			this.windowSelectFetch.dispose();
+			GameDAO gameDAO = new GameDAO();
 			
-			WindowFetch wfn = new WindowFetch(windowSelectFetch);
-			wfn.createWindowName(new InternalFrameName(wfn));
+			Game g = gameDAO.fetchGameById(Integer.valueOf(internalFrameFetch.getFldFetch().getText()));
+			
+			if (g != null) {
+				
+				this.internalFrameFetch.dispose();
+				this.windowFetch.dispose();
+				this.windowSelectFetch.dispose();
+				
+				WindowFetch wfn = new WindowFetch(windowSelectFetch);
+				wfn.createWindowName(new InternalFrameName(wfn, g));
+			}
+			internalFrameFetch.setFldFetch("");
 		}
 	}
 
