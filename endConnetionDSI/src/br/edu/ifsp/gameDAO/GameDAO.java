ca7f16e00bd7a99469ca5b7bbdb6988082a72fca
Arthur Orlando Vaso <eng.arthurvaso@outlook.com>
2022-01-06 19:54:07 -0300

    Added tries to catch exception and a new function to list all entities in DB

--- endConnetionDSI/src/br/edu/ifsp/gameDAO/GameDAO.java
+++ endConnetionDSI/src/br/edu/ifsp/gameDAO/GameDAO.java
@@ -1,52 +1,61 @@
 package br.edu.ifsp.gameDAO;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import javax.persistence.PersistenceException;
+import javax.persistence.RollbackException;
+
 import javax.swing.JOptionPane;
 
 import br.edu.ifsp.model.Game;
 
 public class GameDAO {
-	private EntityManager geteEntityManager() {
+
+	private EntityManager getEntityManager() {
 		EntityManagerFactory emf = null;
 		EntityManager em = null;
+
+		emf = Persistence.createEntityManagerFactory("databasePU");
+		em = emf.createEntityManager();
 
 		return em;
 	}
 
 	public Game saveGame(Game g) {
-		EntityManager em = geteEntityManager();
+		EntityManager em = getEntityManager();
+
 		try {
 			em.getTransaction().begin();
 
 			if (g.getId() == null) {
-				em.persist(g);
+				try {
+					em.persist(g);
+				} catch (PersistenceException de) {
+					JOptionPane.showMessageDialog(null, "Algum campo excedeu o limite de caracteres.");
+				g = null;
+				}
 			} else {
 				em.merge(g);
+			}
+			try {
+				em.getTransaction().commit();
+			} catch (RollbackException rbe) {
+				JOptionPane.showMessageDialog(null, "Algum campo excedeu o limite de caracteres.");
+				g = null;
 			}
 		} finally {
 			em.close();
 		}
-
 		return g;
 	}
-	
+
 	public void removeGame(Integer id) {
-		EntityManager em = geteEntityManager();
+		EntityManager em = getEntityManager();
 		Game remove = em.find(Game.class, id);
-		
-		try {
-			em.getTransaction().begin();
-			em.remove(remove);
-			em.getTransaction().commit();
-		} finally {
-			em.close();
-		}
-	}
-
-	public void removeGame(String name) {
-		EntityManager em = geteEntityManager();
-		Game remove = em.find(Game.class, name);
 
 		try {
 			em.getTransaction().begin();
@@ -55,37 +64,32 @@ public class GameDAO {
 		} finally {
 			em.close();
 		}
-	}
-	
-	public Game fetchGameById(String name) {
-		EntityManager em = geteEntityManager();
-		Game recovery = null;
-		
-		try {
-			recovery = em.find(Game.class, name);
-			recovery.getTitle().isBlank();
-		}catch (NullPointerException npe) {
-			JOptionPane.showMessageDialog(null, "Título não encontrado.");
-		}finally {
-			em.close();
-		}
-		
-		return recovery;
 	}
 
 	public Game fetchGameById(Integer id) {
-		EntityManager em = geteEntityManager();
+		EntityManager em = getEntityManager();
 		Game recovery = null;
-		
+
 		try {
 			recovery = em.find(Game.class, id);
 			recovery.getTitle().isBlank();
-		}catch (NullPointerException npe) {
+		} catch (NullPointerException npe) {
 			JOptionPane.showMessageDialog(null, "Título não encontrado.");
-		}finally {
+		} finally {
 			em.close();
 		}
-		
+
 		return recovery;
+	}
+
+	public List<Game> fetchPersonByName() {
+		EntityManager em = getEntityManager();
+
+		List<Game> list = null;
+
+		list = (List<Game>) em.createQuery("from Game").getResultList();
+
+		return list;
+
 	}
 }
