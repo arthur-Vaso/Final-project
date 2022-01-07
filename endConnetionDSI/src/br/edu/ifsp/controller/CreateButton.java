38bd1d1d85f00fdf5cf44c6c8f3ec9e5dc724a71
Arthur Orlando Vaso <eng.arthurvaso@outlook.com>
2022-01-07 14:30:41 -0300

    Added a condition to set the icon for the button.

--- endConnetionDSI/src/br/edu/ifsp/controller/CreateButton.java
+++ endConnetionDSI/src/br/edu/ifsp/controller/CreateButton.java
@@ -2,20 +2,26 @@ package br.edu.ifsp.controller;
 
 import java.awt.Font;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
 public class CreateButton {
 
-	public JButton setButton(JButton button, String text, String font, int style, int size) {
+	public JButton setButton(JButton button, String text, String font, int style, int size, String url) {
 		button = new JButton(text);
 		button.setFont(new Font(font, style, size));
-		
+		try {
+			button.setIcon(new ImageIcon(getClass().getResource(url)));
+		} catch (NullPointerException npe) {
+			// TODO: handle exception
+		}
 		return button;
 	}
 
-	public JButton setButton(JButton button, String text) {
+	public JButton setButton(JButton button, String text, String url) {
 		button = new JButton(text);
-		
+		button.setIcon(new ImageIcon(getClass().getResource(url)));
+
 		return button;
 	}
 
